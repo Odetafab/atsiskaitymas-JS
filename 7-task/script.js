69a0f75e2960ee1dfda566b888d40eb2f19a3909
odetafab <odetafabijonaviciute4@gmail.com>
2022-04-19 22:08:27 +0300

    7-task started

---
+++ 7-task/script.js
@@ -0,0 +1,13 @@
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+function showObjectKeys {
+  
+}
+
+console.log(Object.keys(object1));
+// expected output: Array ["a", "b", "c"]
