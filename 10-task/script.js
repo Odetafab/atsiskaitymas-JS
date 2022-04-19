63e104078c714112fd3daf0e450bc2f56cf5a827
odetafab <odetafabijonaviciute4@gmail.com>
2022-04-19 22:56:51 +0300

    added all the tasks

--- 10-task/script.js
+++ 10-task/script.js
@@ -1,1 +1,14 @@
 
+/* ------------------------------ TASK 9 ---------------------------------------------------
+Sutvarkykite uÅ¾duoties "Task 10" esanÄ¨ius failus taip, kad veiktÅ³ Å¾emiau pateiktos funcijos
+-------------------------------------------------------------------------------------------- */
+
+let a = composition(one, four);
+let b = division(four, two);
+let c = substraction(three, two);
+let d = multiplication(five, two);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(c);
